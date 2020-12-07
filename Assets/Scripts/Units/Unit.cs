8ee794817ef90f6156dd2334bd9ccc10ea94754c
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-06 19:42:28 -0500

    Lesson 17 - only moving selected units
    Adding some new code and a bit of refactoring so that now only the selected unit moves

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -6,8 +6,14 @@ using UnityEngine.Events;
 
 public class Unit : NetworkBehaviour
 {
+    [SerializeField] private UnitMovement unitMovement = null;
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
 
     #region Client
     [Client]
