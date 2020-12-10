05a7b9bb88d11fc7ceff2241f866a29d08bb9d9d
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-09 20:44:25 -0500

    Section 18 - Player Unit Lists
    Created ability for the player to have a list of units. The server also keeps track of these lists for all players. The event subscription thing was slightly confusing, but I think I have an overall gist.

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,73 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitsSpawned; // subscribing to an event
+        Unit.ServerOnUnitDespawned += ServerHandleUnitsDespawned; // subscribing to an event
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitsSpawned; // unsubscribing to an event
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitsDespawned; // unsubscribing to an event
+    }
+
+    private void ServerHandleUnitsSpawned(Unit unit)
+    {
+        if(unit.connectionToClient.connectionId != connectionToClient.connectionId) { return;  }
+
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitsDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly) { return; }
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitsSpawned; // subscribing to an event
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitsDespawned; // subscribing to an event
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitsSpawned; // subscribing to an event
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitsDespawned; // subscribing to an event
+    }
+
+    private void AuthorityHandleUnitsSpawned(Unit unit)
+    {
+        if(!hasAuthority) { return; }
+
+        myUnits.Add(unit);
+    }
+
+    private void AuthorityHandleUnitsDespawned(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        myUnits.Remove(unit);
+    }
+
+
+    #endregion
+
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
 
 public class Unit : NetworkBehaviour
 {
@@ -10,12 +11,47 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned; // this can be invoked 
+    public static event Action<Unit> ServerOnUnitDespawned; // this can be invoked 
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+    #endregion
+
     #region Client
+
+    public override void OnStartClient()
+    {
+        if(!isClientOnly ||  !hasAuthority) { return; }
+        
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
     [Client]
     public void Select()
     {
