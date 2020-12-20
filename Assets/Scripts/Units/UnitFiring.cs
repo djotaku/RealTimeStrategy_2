ecee1e9cc90daf207c17ae21052dcefb8ecfea1b
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-19 20:10:22 -0500

    Section 24 - Firing
    I can now shoot. It just goes through the target, but I can shoot!

---
+++ Assets/Scripts/Units/UnitFiring.cs
@@ -0,0 +1,50 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFiring : NetworkBehaviour
+{
+    [SerializeField] private Targeter targeter = null;
+    [SerializeField] private GameObject projectilePrefab = null;
+    [SerializeField] private Transform projectileSpawnPoint = null; // so that it comes out of the barrel of the gun, not the middle of the unit
+    [SerializeField] private float fireRange = 5f;
+    [SerializeField] private float fireRate = 1f;
+    [SerializeField] private float rotationSpeed = 20f;
+
+    private float lastFireTime;
+
+    [ServerCallback]
+    private void Update()
+    {
+        Targetable target = targeter.GetTarget();
+
+        if(target == null) { return;  }
+
+        if(!CanFireAtTarget()) { return; }
+
+        // if we are here, we are in range. Now we want to face
+
+        Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); // using rotation speed * time.delta helps with lag
+
+        if(Time.time > (1 / fireRate) + lastFireTime)
+        {
+
+            Quaternion projectileRotation = Quaternion.LookRotation(target.GetAimAtPoint().position - projectileSpawnPoint.position);
+
+            GameObject projectileInstance = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileRotation);
+
+            NetworkServer.Spawn(projectileInstance, connectionToClient);
+
+            lastFireTime = Time.time;
+        }
+    }
+
+    [Server]
+    private bool CanFireAtTarget()
+    {
+        return (targeter.GetTarget().transform.position - transform.position).sqrMagnitude <= fireRange * fireRange;
+    }    
+}
---
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitProjectile : NetworkBehaviour
+{
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private float destroyAfterSeconds = 5f;
+    [SerializeField] private float lauchForce = 10f;
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * lauchForce;
+    }
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [Server]
+    private void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+}
