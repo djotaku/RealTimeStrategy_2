b1e206a00af2bf206daa7ab39bb6675241c1fb0a
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-21 16:30:10 -0500

    Section 25 - Dealing Damage
    We can now deal damage. Nothing actually happens to the tanks, but they get damage dealt.

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,43 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar]
+    private int currentHealth;
+
+    public event Action ServerOnDie;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        if(currentHealth == 0) { return;  } // only die once!
+
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0); // keeps it from going negative and saves a bunch of code
+        
+        if(currentHealth != 0) { return; }
+
+        ServerOnDie?.Invoke(); // this raises an event that can be listened to elsewhere so each object can have a different death
+
+        Debug.Log("We died.");
+    }
+
+    #endregion
+
+    #region Cleint
+
+    #endregion
+
+}
--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damateToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float lauchForce = 10f;
 
@@ -17,6 +18,22 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback] // don't call on the client
+    private void OnTriggerEnter(Collider other) // called when this item collides with soemthing else
+    {
+        if(other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if(networkIdentity.connectionToClient == connectionToClient) { return; }  // don't allow your units to hurt each other
+        }
+
+        if(other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damateToDeal);
+        }
+
+        DestroySelf(); // get rid of the projectile
     }
 
     [Server]
