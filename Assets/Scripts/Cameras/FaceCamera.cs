d257bf416ce6d9e719a948e300690416422cc31f
Eric Mesa <ericsbinaryworld@gmail.com>
2021-01-05 18:57:09 -0500

    Section 31 - Setting up for building placement
    The first half of this lecture was putting into place all the layers and the physics matrix.
    
    The second half was about making sure the health bars always face the camera.

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // not using update because we want ot make sure the camera has rotated before we do this
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.transform.rotation * Vector3.up);
+    }
+
+}
